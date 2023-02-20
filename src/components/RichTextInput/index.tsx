d7a6a57b03a70eed2efb564d80170e70c2588ec1
LeePeter <74251905+desiyc@users.noreply.github.com>
2023-02-20 16:44:43 +0800

    fix 富文本组件优化

--- src/components/RichTextInput/index.tsx
+++ src/components/RichTextInput/index.tsx
@@ -1,69 +1,52 @@
 import { UPLOAD_SERVER_HOME } from '../../common/config'
+import '@wangeditor/editor/dist/css/style.css'
+import { useState, useEffect } from 'react'
+import { Editor, Toolbar } from '@wangeditor/editor-for-react'
+import { IDomEditor, IEditorConfig, IToolbarConfig } from '@wangeditor/editor'
 import { message } from 'antd'
 import { requestFile } from 'dva17'
-import ReactWEditor from 'wangeditor-for-react'
 
-export default function RichTextInput(props: any) {
-  const { placeholder, value, onChange, defaultValue }: any = props
+type InsertFnType = (url: string, alt: string, href: string) => void
+
+export default (props: any) => {
+  const { value, onChange }: any = props
+  const [editor, setEditor]: any = useState<IDomEditor | null>(null)
+  // 工具栏配置
+  const toolbarConfig: Partial<IToolbarConfig> = {
+    excludeKeys: ['group-video', 'insertImage'],
+  }
+  // 编辑器配置
+  const editorConfig: Partial<IEditorConfig> = {
+    placeholder: '请输入内容...',
+    MENU_CONF: {
+      uploadImage: {
+        async customUpload(file: File, insertFn: InsertFnType) {
+          message.loading('图片上传中..', 0)
+          const { url } = await requestFile(UPLOAD_SERVER_HOME, file)
+          insertFn(url, url, url)
+          message.destroy()
+        },
+      },
+    },
+  }
+  // 及时销毁 editor ，重要！
+  useEffect(() => {
+    return () => {
+      if (editor == null) return
+      editor.destroy()
+      setEditor(null)
+    }
+  }, [editor])
   return (
-    <div className="braft_editor">
-      <ReactWEditor
-        defaultValue={defaultValue}
-        placeholder={placeholder}
+    <div>
+      <Toolbar editor={editor} defaultConfig={toolbarConfig} mode="default" style={{ borderBottom: '1px solid #ccc' }} />
+      <Editor
+        defaultConfig={editorConfig}
         value={value}
-        onChange={(newHtml: any) => {
-          onChange && onChange({ newHtml })
-        }}
-        config={{
-          pasteFilterStyle: false,
-          uploadFileName: 'file',
-          uploadImgMaxLength: 5,
-          uploadImgAccept: ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp'],
-          showLinkImg: false,
-          //关闭插入网络视频按钮
-          showLinkVideo: false,
-          uploadImgShowBase64: true,
-          //自定义图片上传
-          customUploadImg: async (resultFiles: Array<File>, insertImgFn: Function) => {
-            for (let index = 0; index < resultFiles.length; index++) {
-              const file = resultFiles[index]
-              try {
-                message.loading('图片上传中', 0)
-                let img = await requestFile(UPLOAD_SERVER_HOME, file)
-                if (img) {
-                  //如果接口返回值，销毁上一条loading状态message
-                  message.destroy()
-                }
-                let { url } = img
-                message.success('图片上传成功！')
-                insertImgFn(url)
-              } catch (e) {
-                message.error('图片上传异常，请重试！')
-              }
-            }
-            return false
-          },
-          //自定义上传视频
-          customUploadVideo: async (resultFiles: Array<File>, insertVideoFn: Function) => {
-            for (let index = 0; index < resultFiles.length; index++) {
-              const file = resultFiles[index]
-              try {
-                message.loading('视频上传中', 0)
-                let video = await requestFile(UPLOAD_SERVER_HOME, file)
-                if (video) {
-                  //如果接口返回值，销毁上一条loading状态message
-                  message.destroy()
-                }
-                let { url } = video
-                message.success('视频上传成功！')
-                insertVideoFn(url)
-              } catch (e) {
-                message.error('视频上传异常，请重试！')
-              }
-            }
-            return false
-          },
-        }}
+        onCreated={setEditor}
+        onChange={(currentEditor: any) => onChange(currentEditor.getHtml())}
+        mode="default"
+        style={{ height: '500px', overflowY: 'hidden' }}
       />
     </div>
   )
